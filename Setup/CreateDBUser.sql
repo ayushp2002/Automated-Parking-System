509add5dedb1b0388138dada47ee9712d9bbc85a
Ayush Priyadarshi <31592771+ayushp2002@users.noreply.github.com>
2021-01-11 10:57:32 +0530

    Add files via upload

---
+++ Setup/CreateDBUser.sql
@@ -0,0 +1,5 @@
+CREATE USER javadev IDENTIFIED BY password;
+
+GRANT ALL PRIVILEGES TO javadev;
+
+EXIT;
--- Setup/GenerateTable.sql
+++ Setup/GenerateTable.sql
@@ -21,10 +21,11 @@ CREATE TABLE booking (
 	cost NUMBER
 );
 
+/*
 INSERT ALL
 	INTO users VALUES ('ak289', 	'ak289pass', 	'Ayush Kumar', 		'8920367342',		 	'ayushkumar@gmail.com')
 	INTO users VALUES ('ys978', 	'ys978pass', 	'Yash Sankhla', 		'1234567890',	 	'yashsankhla@gmail.com')
 	INTO users VALUES ('mv102', 	'mv102pass', 	'Manan Verma', 		'0987654321',	 		'mananverma@gmail.com')
 	INTO users VALUES ('ds712', 	'ds712pass', 	'Divyansh Sharma', '0147852369',	 	'divyashsharma@gmail.com')
 SELECT * FROM DUAL;
-
+*/
